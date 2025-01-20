cd3163379c9bac6e768eca76e5ff8d228b898235
Matvey Baryshev <baryshev.matvey@yandex.ru>
2025-01-20 17:33:33 +0300

    feat: icon component with few icons from design

---
+++ src/shared/ui/Icons/Icon.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import * as Icons from "./svg";
+
+export const icons = {
+  account: Icons.Account,
+  bell: Icons.Bell,
+  bellBadge: Icons.BellBadge,
+  checkboxBlank: Icons.CheckboxBlank,
+};
+
+type IconProps = {
+  name: keyof typeof icons;
+  size?: number;
+  color?: string;
+};
+
+export const Icon: React.FC<IconProps> = ({ name, size = 24, color = "currentColor" }) => {
+  const IconComponent = icons[name];
+
+  if (!IconComponent) {
+    console.warn(`Icon "${name}" not found.`);
+    return null;
+  }
+
+  return (
+    <IconComponent size={size} color={color}/>
+  );
+};
---
+++ src/shared/ui/Icons/svg/index.ts
@@ -0,0 +1,5 @@
+export {Account} from "./account";
+export {Bell} from "./bell";
+export {BellBadge} from "./bell-badge";
+export {CheckboxBlank} from "./checkbox-blank";
+
