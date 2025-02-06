cefeb29e310da0b96b036817a6b9f5d4ed85ab39
Matvey Baryshev <matvey33.baryshev@mail.ru>
2025-02-06 22:00:12 +0300

    feat: refactor profile widgets structure

---
+++ src/widgets/profile/competitions/index.ts
@@ -0,0 +1,4 @@
+export {CompetitionsCreate} from "./ui/CompetitionsCreate";
+export {Competitions} from "./ui/Competitions";
+
+export {useCompetitionsStore} from "./model/CompetitionsStore";
---
+++ src/widgets/profile/competitions/ui/Competitions.tsx
@@ -0,0 +1,65 @@
+import React, {useEffect} from "react";
+import {useCompetitionsStore} from "@/widgets/profile";
+import {CompetitionCard} from "@/entities/competition";
+import {Button} from "@/shared/ui";
+
+interface CompetitionsProps {
+  role: "sportsman" | "organization";
+}
+
+export const Competitions: React.FC<CompetitionsProps> = ({
+  role,
+}) => {
+  const passed = useCompetitionsStore((state) => state.passed);
+  const upcoming = useCompetitionsStore((state) => state.upcoming);
+  const getCompetitions = useCompetitionsStore((state) => state.getCompetitions);
+  
+  useEffect(() => {
+    getCompetitions();
+  }, [getCompetitions])
+  return (
+    <div className={"flex flex-col w-full h-full gap-4 items-center"}>
+      <label className={"text-h5_bold text-base-95 text-center"}>Соревнования</label>
+
+      <div className={"flex h-1 w-full bg-base-5"}/>
+
+      {role == "organization" && (
+        <div className={"flex flex-row w-full"}>
+          <div className={"flex flex-col w-full"}>
+            <p className={"text-bodyM_regular text-base-95 text-center"}>Проведено стартов</p>
+            <p className={"text-h5_bold text-blue-80 text-center"}>10</p>
+          </div>
+
+          <div className={"flex flex-col w-full"}>
+            <p className={"text-bodyM_regular text-base-95 text-center"}>Среднее количество участников</p>
+            <p className={"text-h5_bold text-blue-80 text-center"}>65</p>
+          </div>
+        </div>
+      )}
+
+      {/* UPCOMING */}
+      <div className={"flex flex-col w-full gap-4 items-center"}>
+        <label className={"w-full text-bodyM_medium text-base-95"}>Предстоящие</label>
+
+        {upcoming.map((comp) => (
+          <CompetitionCard key={comp.competitionUuid} competition={comp} />
+        ))}
+
+        {role == "organization" && (
+          <Button variant={"secondary"} size={"M"} rightIcon={"plus"} className={"w-full max-w-[350px]"}>
+            Добавить соревнование
+          </Button>
+        )}
+      </div>
+
+      {/* PASSED */}
+      <div className={"flex flex-col w-full gap-4"}>
+        <label className={"text-bodyM_medium text-base-95"}>Прошедшие</label>
+
+        {passed.map((comp) => (
+          <CompetitionCard key={comp.competitionUuid} competition={comp} />
+        ))}
+      </div>
+    </div>
+  )
+}
