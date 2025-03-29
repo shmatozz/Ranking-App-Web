bb61d4385c2349cab37edc03e198060754cce51d
Matvey Baryshev <baryshev.matvey@yandex.ru>
2025-03-29 09:25:27 +0300

    feat: add partners carousel to main page

--- src/widgets/main-page-sections/ui/Partners.tsx
+++ src/widgets/main-page-sections/ui/Partners.tsx
@@ -1,22 +1,28 @@
-import React from "react";
+"use client"
+
+import React, {useEffect} from "react";
+import Marquee from "react-fast-marquee";
+import {useAboutInfoStore} from "@/widgets/about-page-sections";
+import {PartnerCard} from "@/features/about-info";
 
 export const Partners = () => {
-  return (
-    <div className={"content-container flex-col gap-4"}>
-      <label className={"text-h4 text-base-95"}>Партнеры</label>
+  const { partners, getPartners } = useAboutInfoStore();
 
-      <span className={"text-bodyM_regular text-base-95 whitespace-pre-wrap"}>
-          <li>{"ООО НФГ \"Сеть фитнес клубов Физкульт\""}</li>
-          <li>{"Организатор заплывов и соревнований «Ночная Лига»"}</li>
-          <li>{"ФПНО - Федерация Плавания Нижегородской Области"}</li>
-          <li>{"ФПОНО - федерация плавания в открытой воде Нижегородской Области"}</li>
-          <li>{"ФПМ - федерация плавания мастерс"}</li>
-          <li>{"РОО ФПНО - РЕГИОНАЛЬНАЯ ОБЩЕСТВЕННАЯ ОРГАНИЗАЦИЯ \"ФЕДЕРАЦИЯ ЗИМНЕГО ПЛАВАНИЯ НИЖЕГОРОДСКОЙ ОБЛАСТИ\""}</li>
-          <li>{"АНО Виктория Центр"}</li>
-          <li>{"ФТНО - федерация триллиона нижегородской области"}</li>
-          <li>{"Триатлон клуб - Триатлон НН"}</li>
-          <li>{"Беговой клуб - DiM TeaM"}</li>
-        </span>
+  useEffect(() => {
+    if (!partners) getPartners();
+  }, [partners, getPartners]);
+
+  if (!partners) return null
+
+  return (
+    <div className={"flex h-full w-full max-w-[1100px] m-auto rounded-3xl bg-base-0 lg-md:shadow-[0_4px_16px_0px_rgba(0,0,0,0.08)] lg-md:py-8 flex-col gap-4"}>
+      <label className={"text-h4 text-base-95 px-[24px] xs:px-[52px]"}>Партнеры</label>
+
+      <Marquee pauseOnHover gradient>
+        {partners.map((partner) => (
+          <PartnerCard key={partner.id} partner={partner} classname={"mx-[16px] w-[200px] h-[200px]"}/>
+        ))}
+      </Marquee>
     </div>
   )
 }
