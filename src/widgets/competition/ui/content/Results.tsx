0e7c58f42d2a55bd4a7250209fed6b6c67dcc41f
Matvey Baryshev <baryshev.matvey@yandex.ru>
2025-04-11 09:41:30 +0300

    fix: results form layout

--- src/widgets/competition/ui/content/Results.tsx
+++ src/widgets/competition/ui/content/Results.tsx
@@ -80,77 +80,87 @@ export const Results = () => {
       </div>
 
       {resultsFormVisible && (
-        <div className="hidden flex-col fixed inset-0 z-50 bg-white overflow-auto p-6 lg:flex">
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-xl font-semibold">Ручной ввод результатов</h2>
-            <button
-              onClick={() => setResultsFormVisible(false)}
-              className="text-gray-500 hover:text-black text-2xl font-bold"
-            >
-              &times;
-            </button>
-          </div>
+        <>
+          <div
+            className="fixed inset-0 z-60 bg-base-95 bg-opacity-50"
+            onClick={() => setResultsFormVisible(false)}
+          />
 
-          <div className="flex flex-col gap-4">
-            {usersFull?.map((user) => (
-              <div
-                key={user.email}
-                className="grid grid-cols-2 md:grid-cols-6 gap-2 items-center border-b pb-2"
+          <div
+            className="hidden flex-col fixed inset-0 z-70 max-w-[1100px] m-auto my-24 container-shadow rounded-2xl bg-white overflow-auto p-6 lg:flex">
+
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-xl font-semibold">Ручной ввод результатов</h2>
+              <button
+                onClick={() => setResultsFormVisible(false)}
+                className="text-gray-500 hover:text-black text-2xl font-bold"
               >
-                <span className="col-span-2">{`${user.lastName} ${user.firstName} ${user.middleName} - ${user.email}`}</span>
+                &times;
+              </button>
+            </div>
 
-                {["hour", "minute", "second", "nano"].map((unit) => (
-                  <input
-                    key={unit}
-                    type="number"
-                    min={0}
-                    max={unit === "hour" ? 23 : 59}
-                    placeholder={unit == "hour" ? "Часы" : (unit == "minute" ? "Минуты" : (unit == "second" ? "Секунды" : "Милисекунды"))}
-                    className="border rounded p-1 w-full"
-                    onChange={(e) => {
-                      const value = parseInt(e.target.value);
-                      setResultsInputs((prev) => {
-                        const existing = prev.find((res) => res.userEmail === user.email);
-                        const updated = {
-                          userEmail: user.email,
-                          distance: swimDistance,
-                          gender: user.gender,
-                          userTime: {
-                            hour: existing?.userTime.hour ?? 0,
-                            minute: existing?.userTime.minute ?? 0,
-                            second: existing?.userTime.second ?? 0,
-                            nano: 0,
-                            ...(existing?.userTime || {})
-                          },
-                          ...existing
-                        };
+            <div className="flex flex-col gap-4">
+              {usersFull?.map((user) => (
+                <div
+                  key={user.email}
+                  className="grid grid-cols-2 md:grid-cols-6 gap-2 items-center border-b pb-2"
+                >
+            <span
+              className="col-span-2">{`${user.lastName} ${user.firstName} ${user.middleName} - ${user.email}`}</span>
 
-                        updated.userTime[unit as "hour" | "minute" | "second"] = isNaN(value) ? 0 : value;
+                  {["hour", "minute", "second", "nano"].map((unit) => (
+                    <input
+                      key={unit}
+                      type="number"
+                      min={0}
+                      max={unit === "hour" ? 23 : 59}
+                      placeholder={unit == "hour" ? "Часы" : (unit == "minute" ? "Минуты" : (unit == "second" ? "Секунды" : "Милисекунды"))}
+                      className="border rounded p-1 w-full"
+                      onChange={(e) => {
+                        const value = parseInt(e.target.value);
+                        setResultsInputs((prev) => {
+                          const existing = prev.find((res) => res.userEmail === user.email);
+                          const updated = {
+                            userEmail: user.email,
+                            distance: swimDistance,
+                            gender: user.gender,
+                            userTime: {
+                              hour: existing?.userTime.hour ?? 0,
+                              minute: existing?.userTime.minute ?? 0,
+                              second: existing?.userTime.second ?? 0,
+                              nano: 0,
+                              ...(existing?.userTime || {})
+                            },
+                            ...existing
+                          };
 
-                        return [
-                          ...prev.filter((r) => r.userEmail !== user.email),
-                          updated
-                        ];
-                      });
-                    }}
-                  />
-                ))}
-              </div>
-            ))}
+                          updated.userTime[unit as "hour" | "minute" | "second"] = isNaN(value) ? 0 : value;
+
+                          return [
+                            ...prev.filter((r) => r.userEmail !== user.email),
+                            updated
+                          ];
+                        });
+                      }}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
+
+            <Button
+              className="mt-6 w-full max-w-[350px] self-center"
+              onClick={() => {
+                console.log("Sending to backend:", resultsInputs);
+                if (selectedSwim) uploadSwimResultsByForm(selectedSwim.id, resultsInputs)
+                setResultsFormVisible(false);
+              }}
+              disabled={usersFull && resultsInputs.length < usersFull.length}
+            >
+              Отправить результаты
+            </Button>
           </div>
-
-          <Button
-            className="mt-6 w-full max-w-[350px] self-center"
-            onClick={() => {
-              console.log("Sending to backend:", resultsInputs);
-              if (selectedSwim) uploadSwimResultsByForm(selectedSwim.id, resultsInputs)
-              setResultsFormVisible(false);
-            }}
-            disabled={usersFull && resultsInputs.length < usersFull.length}
-          >
-            Отправить результаты
-          </Button>
-        </div>
+        </>
       )}
     </div>
   )
