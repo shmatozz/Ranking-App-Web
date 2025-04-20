de6d76ac1c2ca2c6ea0e213061107a86a83eb66d
Matvey Baryshev <baryshev.matvey@yandex.ru>
2025-04-20 19:49:31 +0300

    feat: stream displaying in competition page

--- src/widgets/competition/ui/content/Live.tsx
+++ src/widgets/competition/ui/content/Live.tsx
@@ -1,10 +1,32 @@
 import React from "react";
+import {useCompetitionStore} from "@/features/competition/get";
+import {getEmbedStreamUrl} from "@/shared/lib";
 
 export const Live = () => {
+  const { competition } = useCompetitionStore();
+  const streamUrl = competition?.videoLink;
+
+  const embedUrl = streamUrl ? getEmbedStreamUrl(streamUrl) : null;
+
   return (
-    <div>
-      <iframe src="https://live.vkvideo.ru/app/embed/fox.media" scrolling="no"
-              height="378" width="620"></iframe>
+    <div className="flex flex-col items-center p-4">
+      {embedUrl ? (
+        <iframe
+          src={embedUrl}
+          scrolling="no"
+          height="378"
+          width="620"
+          allow="autoplay; encrypted-media"
+          allowFullScreen
+          className="rounded shadow-md w-full max-w-3xl aspect-video"
+        />
+      ) : (
+        <div className="text-gray-500 text-center">
+          {streamUrl
+            ? "Невозможно встроить указанную трансляцию"
+            : "Трансляция ещё не добавлена"}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
